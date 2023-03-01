dce8d7c9ecbb0ef3fc2b33eea8ef3901175a8766
Asyraf Hussin <asyrafhussin4@gmail.com>
2023-03-01 12:44:17 +0800

    Update

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,7 +1,6 @@
 import Alpine from "alpinejs";
 import FormsAlpinePlugin from "../../vendor/filament/forms/dist/module.esm";
 import NotificationsAlpinePlugin from "../../vendor/filament/notifications/dist/module.esm";
-import "../../vendor/alperenersoy/filament-export/resources/js/filament-export.js";
 
 Alpine.plugin(FormsAlpinePlugin);
 Alpine.plugin(NotificationsAlpinePlugin);
